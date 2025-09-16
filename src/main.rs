04fe6796381711ad2afe3cb3f561af486d086ac1
Manse <pedro.manse@dmk3.com.br>
2025-09-15 22:59:31 -0300

    pretty errors

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,13 @@ use std::fs::read_to_string;
 
 use compl::CompError;
 
-fn main() -> Result<(), CompError> {
+fn main() {
+    if let Err(e) = exec() {
+        eprintln!("{e}");
+    }
+}
+
+fn exec() -> Result<(), CompError> {
     let mut inputs = std::env::args().skip(1);
 
     let file = read_to_string(inputs.next().ok_or(CompError::MissingComplFile)?)?;
